0ca24f883461e1d7522f07cd62a095700afbfe64
Ruud van der Weide <rj@rdcl.nl>
2019-04-14 14:44:14 +0200

    Added some simple actions

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,51 @@ import { createStore } from 'redux'
 import './style.sass'
 
 // Definitie van de state
-type StateType = {}
-const INITIAL_STATE: StateType = {}
+type StateType = {
+  count: number,
+}
+const INITIAL_STATE: StateType = {
+  count: 0,
+}
 
 // Definities van de mogelijke acties
-type ActionType = any
+type IncrementAction = { type: 'increment' }
+type DecrementAction = { type: 'decrement' }
+
+type ActionType = IncrementAction | DecrementAction
 
 // Opzetten store
 const store = createStore(
   (state: StateType = INITIAL_STATE, action: ActionType): StateType => {
     switch (action.type) {
+      case 'increment':
+        return {
+          ...state,
+          count: state.count + 1,
+        }
+      case 'decrement':
+        return {
+          ...state,
+          count: state.count - 1,
+        }
       default:
         return state
     }
   }
 )
+
+// Registreer acties
+q('.actions > .increment').addEventListener('click', () => {
+  store.dispatch({
+    type: 'increment',
+  })
+})
+
+q('.actions > .decrement').addEventListener('click', () => {
+  store.dispatch({
+    type: 'decrement',
+  })
+})
 
 // Met store.subscribe laat je weten dat je geïnteresseerd bent in wijzigingen aan de state
 store.subscribe(writeStateToDom)
@@ -24,6 +54,9 @@ writeStateToDom()
 
 
 function writeStateToDom() {
-  const output = document.getElementById('output') as HTMLElement
-  output.innerHTML = JSON.stringify(store.getState(), null, 2)
+  q('#output').innerHTML = JSON.stringify(store.getState(), null, 2)
+}
+
+function q(id: string): HTMLElement {
+  return document.querySelector(id) as HTMLElement
 }
